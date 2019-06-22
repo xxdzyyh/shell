03326083d1571879733fb754af23bba0e032201a
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2019-06-22 18:07:36 +0800

    json to model

--- TaoModelToView.sh
+++ TaoModelToView.sh
@@ -7,21 +7,31 @@ grep -B 1 @property $1 | awk 'BEGIN {
 } {
 	propertyname=substr($5,2,length($5)-2);
 	property="_"propertyname;
-	viewName=propertyname"Label";
+	
 
 	if($0!~/@property/) {
 		note=$0;
 	};
 
 	if ($4~/NSString/) {
+		viewName=propertyname"Label";
+
 		declear=declear note"\n@property (nonatomic, strong) UILabel *"viewName";\n";
 		getter=getter"- (UILabel *)"viewName" {\n	if (!_"viewName") {\n		_"viewName" = [[UILabel alloc] init];\n	}\n	return _"viewName";\n}\n\n";
-		subviews=subviews"[self addSubview:self."propertyname"Label];\n";
+		subviews=subviews"[self addSubview:self."viewName"];\n";
 		assign=assign"self."viewName".text =data."propertyname";\n";
-		constraints=constraints note"\n[self."propertyname"Label mas_makeConstraints:^(MASConstraintMaker *make) {\
+		constraints=constraints note"\n[self."viewName" mas_makeConstraints:^(MASConstraintMaker *make) {\
 		\
 		}];\n\n";
-	} 
+	} else if ($4~/UI/) {
+		viewName=propertyname;
+
+		getter=getter"- ("$4" *)"viewName" {\n	if (!_"viewName") {\n		_"viewName" = [["$4" alloc] init];\n	}\n	return _"viewName";\n}\n\n";
+		subviews=subviews"[self addSubview:self."viewName"];\n";
+		constraints=constraints note"\n[self."viewName" mas_makeConstraints:^(MASConstraintMaker *make) {\
+		\
+		}];\n\n";
+	}
 
 } END {
 	print declear;
